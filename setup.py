a05c8ebea05e04e07b30662f48f56f28048cd499
Nicco Vögeli <95473696+angryvoegi@users.noreply.github.com>
2024-08-06 11:03:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        beautifulsoup4~=4.10.0
-        pydantic~=1.9.0
-        requests~=2.27.1
-        pandas~=1.4.1
-        lxml~=5.2.1
+        "beautifulsoup4~=4.10.0",
+        "pydantic~=1.9.0",
+        "requests~=2.27.1",
+        "pandas~=1.4.1",
+        "lxml~=5.2.1",
     ],
 )
